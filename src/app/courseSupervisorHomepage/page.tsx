b8e04ee5f34819178829443b2beb6595c5b18dcb
TheFatCat <74298130+TheFatCat27@users.noreply.github.com>
2023-08-29 17:45:54 +1200

    added some basic stuff to the page

--- src/app/courseSupervisorHomepage/page.tsx
+++ src/app/courseSupervisorHomepage/page.tsx
@@ -1,7 +1,21 @@
+import Sidebar from "../components/Sidebar"
+import StarIcon from '@mui/icons-material/Star';
+
 export default function CSHomepage(){
     return(
         <div>
-            <h1>Test</h1>
+            <div>
+                <Sidebar></Sidebar>
+            </div>
+            <div style={{
+                paddingLeft: 250
+                }}>
+                <div style={{display:"inline-flex"}}>
+                <StarIcon></StarIcon>
+                <p>Dashboard</p>
+                </div>
+                <h1>Welcome, Supervisor</h1>
+            </div>
         </div>
     )
 }
