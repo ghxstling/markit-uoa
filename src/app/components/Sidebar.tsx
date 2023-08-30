db61613eda25dc2c42167fdf5ba5a10a3ce5c3e4
xano <62649990+Zen-ai@users.noreply.github.com>
2023-08-30 20:43:55 +1200

    Add user data to sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,131 +1,154 @@
+'use client'
+
 import {
-  Box,
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-} from "@mui/material";
-import React from "react";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import ArchiveIcon from "@mui/icons-material/Archive";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import SettingsIcon from "@mui/icons-material/Settings";
-import LogoutIcon from "@mui/icons-material/Logout";
-import Link from "next/link";
+    Box,
+    Drawer,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    ListSubheader,
+} from '@mui/material'
+import React from 'react'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import ArchiveIcon from '@mui/icons-material/Archive'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import SettingsIcon from '@mui/icons-material/Settings'
+import LogoutIcon from '@mui/icons-material/Logout'
+import Link from 'next/link'
+import { useSession } from 'next-auth/react'
 
 const linkStyle = {
-  textDecoration: "none",
-  color: "white",
-  width: "100%",
-};
+    textDecoration: 'none',
+    color: 'white',
+    width: '100%',
+}
 
 const IconStyle = {
-  fill: "white",
-};
+    fill: 'white',
+}
 
-const content = (
-  <Box
-    sx={{
-      display: "flex",
-      flexDirection: "column",
-      height: "100%",
-      justifyContent: "space-between",
-      width: "15rem",
-    }}
-  >
-    <List>
-      <ListSubheader
-        sx={{
-          color: "white",
-          fontSize: "1.2rem",
-          backgroundColor: "#00467F",
-        }}
-      >
-        <b>Burkhard W.</b>
-      </ListSubheader>
-      <ListSubheader sx={{ color: "white", backgroundColor: "#00467F" }}>
-        bwue001@aucklanduni.ac.nz
-      </ListSubheader>
-      <ListItem disablePadding sx={{ mt: "1.5rem" }}>
-        <Link href="./" passHref style={linkStyle}>
-          <ListItemButton>
-            <ListItemIcon>
-              <DashboardIcon style={IconStyle} />
-            </ListItemIcon>
-            <ListItemText>Dashboard</ListItemText>
-          </ListItemButton>
-        </Link>
-      </ListItem>
+let content = (
+    username: string | null | undefined,
+    email: string | null | undefined
+) => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+                justifyContent: 'space-between',
+                width: '15rem',
+            }}
+        >
+            <List>
+                <ListSubheader
+                    sx={{
+                        color: 'white',
+                        fontSize: '1.2rem',
+                        backgroundColor: '#00467F',
+                    }}
+                >
+                    <b>{username}</b>
+                </ListSubheader>
+                <ListSubheader
+                    sx={{ color: 'white', backgroundColor: '#00467F' }}
+                >
+                    {email}
+                </ListSubheader>
+                <ListItem disablePadding sx={{ mt: '1.5rem' }}>
+                    <Link href="./" passHref style={linkStyle}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <DashboardIcon style={IconStyle} />
+                            </ListItemIcon>
+                            <ListItemText>Dashboard</ListItemText>
+                        </ListItemButton>
+                    </Link>
+                </ListItem>
 
-      <ListItem disablePadding>
-        <Link href="./" passHref style={linkStyle}>
-          <ListItemButton>
-            <ListItemIcon>
-              <ArchiveIcon style={IconStyle} />
-            </ListItemIcon>
-            <ListItemText>My Applications</ListItemText>
-          </ListItemButton>
-        </Link>
-      </ListItem>
+                <ListItem disablePadding>
+                    <Link href="./" passHref style={linkStyle}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ArchiveIcon style={IconStyle} />
+                            </ListItemIcon>
+                            <ListItemText>My Applications</ListItemText>
+                        </ListItemButton>
+                    </Link>
+                </ListItem>
 
-      <ListItem disablePadding>
-        <Link href="./" passHref style={linkStyle}>
-          <ListItemButton>
-            <ListItemIcon>
-              <NotificationsIcon style={IconStyle} />
-            </ListItemIcon>
-            <ListItemText>Notifications</ListItemText>
-          </ListItemButton>
-        </Link>
-      </ListItem>
+                <ListItem disablePadding>
+                    <Link href="./" passHref style={linkStyle}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <NotificationsIcon style={IconStyle} />
+                            </ListItemIcon>
+                            <ListItemText>Notifications</ListItemText>
+                        </ListItemButton>
+                    </Link>
+                </ListItem>
 
-      <ListItem disablePadding>
-        <Link href="./" passHref style={linkStyle}>
-          <ListItemButton>
-            <ListItemIcon>
-              <SettingsIcon style={IconStyle} />
-            </ListItemIcon>
-            <ListItemText>Settings</ListItemText>
-          </ListItemButton>
-        </Link>
-      </ListItem>
-    </List>
+                <ListItem disablePadding>
+                    <Link href="./" passHref style={linkStyle}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <SettingsIcon style={IconStyle} />
+                            </ListItemIcon>
+                            <ListItemText>Settings</ListItemText>
+                        </ListItemButton>
+                    </Link>
+                </ListItem>
+            </List>
 
-    <List>
-      <ListItem disablePadding sx={{ mb: "1.5rem" }}>
-        <Link href="./" passHref style={linkStyle}>
-          <ListItemButton>
-            <ListItemIcon>
-              <LogoutIcon style={IconStyle} />
-            </ListItemIcon>
-            <ListItemText>Logout</ListItemText>
-          </ListItemButton>
-        </Link>
-      </ListItem>
-    </List>
-  </Box>
-);
+            <List>
+                <ListItem disablePadding sx={{ mb: '1.5rem' }}>
+                    <Link href="./" passHref style={linkStyle}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <LogoutIcon style={IconStyle} />
+                            </ListItemIcon>
+                            <ListItemText>Logout</ListItemText>
+                        </ListItemButton>
+                    </Link>
+                </ListItem>
+            </List>
+        </Box>
+    )
+}
 
 const Sidebar = () => {
-  return (
-    <div>
-      <Drawer
-        sx={{
-          "& .MuiDrawer-paper": {
-            backgroundColor: "#00467F",
-            color: "white",
-          },
-        }}
-        anchor="left"
-        variant="permanent"
-      >
-        {content}
-      </Drawer>
-    </div>
-  );
-};
+    const { data: session } = useSession()
 
-export default Sidebar;
+    let sidebarContent = content('', '')
+
+    if (session && session.user && session.user.name && session.user.email) {
+        const name: string =
+            session.user.name.slice(0, session.user.name.lastIndexOf(' ') + 1) +
+            session.user.name.slice(session.user.name.lastIndexOf(' '))[1] +
+            '.'
+        const email: string = session.user.email
+        sidebarContent = content(name, email)
+    }
+
+    return (
+        <div>
+            <Drawer
+                sx={{
+                    '& .MuiDrawer-paper': {
+                        backgroundColor: '#00467F',
+                        color: 'white',
+                    },
+                }}
+                anchor="left"
+                variant="permanent"
+            >
+                {sidebarContent}
+            </Drawer>
+        </div>
+    )
+}
+
+export default Sidebar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Typography from "@mui/material/Typography";
-import { UserStatus } from "./components/UserStatus";
-import Sidebar from "./components/Sidebar";
+import Typography from '@mui/material/Typography'
+import { UserStatus } from './components/UserStatus'
+import Sidebar from './components/Sidebar'
+import Container from '@mui/material/Container'
 
 export default function Home() {
-  return (
-    <>
-      <Typography variant="h1">Hello World</Typography>
-      <UserStatus />
-      <Sidebar />
-    </>
-  );
+    return (
+        <>
+            <Typography variant="h1">Hello World</Typography>
+            <Container maxWidth="xs">
+                <UserStatus />
+                <Sidebar />
+            </Container>
+        </>
+    )
 }
