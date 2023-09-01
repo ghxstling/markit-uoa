a2f73e0bb0bceb3397a35eb6fb5d6c2277ba2e23
Alex Reid <arei209@aucklanduni.ac.nz>
2023-09-01 20:25:02 +1200

    Importing breadcrumbs into course details page

--- src/app/dashboard/courses/page.tsx
+++ src/app/dashboard/courses/page.tsx
@@ -1,12 +1,18 @@
 'use client'
 import CourseDetails from '../../components/courses/CourseDetails'
 import Sidebar from '../../components/Sidebar'
+import DynamicBreadcrumb from '../../components/DynamicBreadcrumb' // Importing the DynamicBreadcrumb component
 
 export default function CreateCoursePage() {
     return (
         <>
             <Sidebar />
             <div>
+                <div style={{ marginLeft: '300px' }}>
+                    {' '}
+                    {/* Add some left margin */}
+                    <DynamicBreadcrumb />
+                </div>
                 <CourseDetails />
             </div>
         </>
