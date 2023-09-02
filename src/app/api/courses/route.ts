86cf8a16db25d11e1968fff106512e2e7a349aa8
ghxstling <dcho282@aucklanduni.ac.nz>
2023-09-02 18:29:07 +1200

    refactor: moved courseSchema to  ZodSchemas file

--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -1,26 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import CourseRepo from '@/data/courseRepo'
-import { z } from "zod"
-
-const courseSchema = z.object({
-    courseCode: z.string()
-        .toUpperCase()
-        .nonempty({ message: "Please provide the Course Code" }),
-    courseDescription: z.string()
-        .nonempty({ message: "Please provide the Course Description" }),
-    numOfEstimatedStudents: z.number()
-        .int().nonnegative(),
-    numOfEnrolledStudents: z.number()
-        .int().nonnegative(),
-    markerHours: z.number()
-        .int().nonnegative(),
-    markerResponsibilities: z.string()
-        .nonempty({ message: "Please add a Marker Resposibilities Description" }),
-    needMarkers: z.boolean(),
-    markersNeeded: z.number()
-        .int().nonnegative(),
-    semester: z.string(),
-}).required()
+import { courseSchema } from '@/app/components/courses/ZodSchemas'
 
 // POST /api/courses/ 
 export async function POST(req: NextRequest) {
