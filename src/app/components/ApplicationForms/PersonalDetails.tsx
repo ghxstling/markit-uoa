cd1fb1ab075afdb4cf72af0773cf198dea4267e2
xano <62649990+Zen-ai@users.noreply.github.com>
2023-09-03 22:04:53 +1200

    Add Applications page, Personal Details, and Employment Details

---
+++ src/app/components/ApplicationForms/PersonalDetails.tsx
@@ -0,0 +1,155 @@
+import {
+    Box,
+    Button,
+    Grid,
+    TextField,
+    Typography,
+    Snackbar,
+} from '@mui/material'
+import { useSession } from 'next-auth/react'
+import React, { useState } from 'react'
+import MuiAlert, { AlertProps } from '@mui/material/Alert'
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
+
+const PersonalDetails = () => {
+    const [openSnackBar, setOpenSnackBar] = useState(false)
+
+    const handleClose = (
+        event?: React.SyntheticEvent | Event,
+        reason?: string
+    ) => {
+        if (reason === 'clickaway') {
+            return
+        }
+
+        setOpenSnackBar(false)
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const data = new FormData(event.currentTarget)
+
+        const studentID = data.get('studentID') as string
+
+        // Check if the length of the studentID is not 9, show error message
+        if (studentID.length !== 9) {
+            setOpenSnackBar(true)
+            return
+        }
+
+        console.log({
+            name: data.get('name'),
+            upi: data.get('upi'),
+            studentID: studentID,
+            email: data.get('email'),
+        })
+    }
+
+    const { data: session } = useSession()
+
+    let name: string = ''
+    let upi: string = ''
+    let email: string = ''
+
+    if (session && session.user && session.user.name && session.user.email) {
+        name = session.user.name
+        email = session.user.email
+        upi = email.slice(0, 7)
+    }
+
+    return (
+        <>
+            <Typography component="h1" variant="h5">
+                Personal Details
+            </Typography>
+            <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 3 }}
+            >
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <TextField
+                            name="name"
+                            required
+                            fullWidth
+                            id="name"
+                            label="Full Name"
+                            autoFocus
+                            value={name}
+                            disabled
+                        />
+                        <input type="hidden" name="name" value={name} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            name="upi"
+                            required
+                            fullWidth
+                            id="upi"
+                            label="UPI"
+                            autoFocus
+                            value={upi}
+                            disabled
+                        />
+                        <input type="hidden" name="upi" value={upi} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            name="email"
+                            required
+                            type="email"
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            value={email}
+                            disabled
+                        />
+                        <input type="hidden" name="email" value={email} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            required
+                            fullWidth
+                            name="studentID"
+                            label="Student ID"
+                            id="studentID"
+                            type="number"
+                        />
+                    </Grid>
+                </Grid>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Submit Details
+                </Button>
+            </Box>
+            <Snackbar
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                open={openSnackBar}
+                autoHideDuration={6000}
+                onClose={handleClose}
+            >
+                <Alert
+                    onClose={handleClose}
+                    severity="error"
+                    sx={{ width: '100%' }}
+                >
+                    Please enter only 9 digits for your student ID
+                </Alert>
+            </Snackbar>
+        </>
+    )
+}
+
+export default PersonalDetails
---
+++ src/app/studentHomepage/Application/page.tsx
@@ -0,0 +1,123 @@
+'use client'
+
+import CVAndTranscript from '@/app/components/ApplicationForms/CVAndTranscript'
+import EmploymentDetails from '@/app/components/ApplicationForms/EmploymentDetails'
+import PersonalDetails from '@/app/components/ApplicationForms/PersonalDetails'
+import Sidebar from '@/app/components/Sidebar'
+import {
+    Box,
+    Button,
+    Container,
+    Paper,
+    Step,
+    StepLabel,
+    Stepper,
+    Typography,
+} from '@mui/material'
+import React from 'react'
+
+const steps = [
+    'Personal Details',
+    'Employment Details',
+    'CV and Academic Transcript Upload',
+]
+
+function getStepContent(step: number) {
+    switch (step) {
+        case 0:
+            return <PersonalDetails />
+        case 1:
+            return <EmploymentDetails />
+        case 2:
+            return <CVAndTranscript />
+        default:
+            throw new Error('Unknown step')
+    }
+}
+
+const Application = () => {
+    const [activeStep, setActiveStep] = React.useState(0)
+
+    const handleNext = () => {
+        setActiveStep(activeStep + 1)
+    }
+
+    const handleBack = () => {
+        setActiveStep(activeStep - 1)
+    }
+
+    return (
+        <div>
+            <>
+                <Box sx={{ display: 'flex' }}>
+                    {/* Create basic layout */}
+                    <Box sx={{ width: '15rem' }}>
+                        <Sidebar />
+                    </Box>
+                    <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+                        <Paper
+                            variant="outlined"
+                            sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+                        >
+                            <Typography
+                                component="h1"
+                                variant="h4"
+                                align="center"
+                            >
+                                Application
+                            </Typography>
+                            <Stepper
+                                activeStep={activeStep}
+                                sx={{ pt: 3, pb: 5 }}
+                            >
+                                {steps.map((label) => (
+                                    <Step key={label}>
+                                        <StepLabel>{label}</StepLabel>
+                                    </Step>
+                                ))}
+                            </Stepper>
+                            {activeStep === steps.length ? (
+                                //Add a snackbar to this later
+                                <>
+                                    <Typography variant="h5" gutterBottom>
+                                        Thank you for your application.
+                                    </Typography>
+                                </>
+                            ) : (
+                                <>
+                                    {getStepContent(activeStep)}
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            justifyContent: 'flex-end',
+                                        }}
+                                    >
+                                        {activeStep !== 0 && (
+                                            <Button
+                                                onClick={handleBack}
+                                                sx={{ mt: 3, ml: 1 }}
+                                            >
+                                                Back
+                                            </Button>
+                                        )}
+                                        <Button
+                                            variant="contained"
+                                            onClick={handleNext}
+                                            sx={{ mt: 3, ml: 1 }}
+                                        >
+                                            {activeStep === steps.length - 1
+                                                ? 'Submit Application'
+                                                : 'Next'}
+                                        </Button>
+                                    </Box>
+                                </>
+                            )}
+                        </Paper>
+                    </Container>
+                </Box>
+            </>
+        </div>
+    )
+}
+
+export default Application
