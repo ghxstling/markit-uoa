4781d4530f7ce2226447bf5beb1dbe7daaa65e84
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2023-09-03 18:56:20 +1200

    feat: add callback to create user on first login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,12 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import UserRepo from '@/data/userRepo'
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
-const clientId = process.env.GOOGLE_CLIENT_ID;
-const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const clientId = process.env.GOOGLE_CLIENT_ID
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET
 
-if (!clientId) throw new Error("Missing env.GOOGLE_CLIENT_ID");
-if (!clientSecret) throw new Error("Missing env.GOOGLE_CLIENT_SECRET");
+if (!clientId) throw new Error('Missing env.GOOGLE_CLIENT_ID')
+if (!clientSecret) throw new Error('Missing env.GOOGLE_CLIENT_SECRET')
 
 const handler = NextAuth({
     providers: [
@@ -14,6 +15,18 @@ const handler = NextAuth({
             clientSecret,
         }),
     ],
-});
+    callbacks: {
+        async signIn({ user }) {
+            const userFromEmail = await UserRepo.getUserbyEmail(user.email!)
+            if (userFromEmail == null) {
+                await UserRepo.createUser({
+                    email: user.email!,
+                    name: user.name!,
+                })
+            }
+            return true
+        },
+    },
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
