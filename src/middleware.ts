c6bef23b13b8166acd44ba6dc5227e83f527a458
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2023-09-04 12:38:23 +1200

    fix: returns 401 when going to auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,18 @@ import { getToken } from 'next-auth/jwt'
 
 const secret = process.env.NEXTAUTH_SECRET
 
+if (secret == null) throw new Error('Missing env.NEXTAUTH_SECRET')
+
 // Limit the middleware to paths starting with `/api/`
 export const config = {
     matcher: '/api/:function*',
 }
 
 export async function middleware(req: NextRequest) {
-    // Call our authentication function to check the request
+    // middlware does not apply to auth routes
+    if (req.nextUrl.pathname.startsWith('/api/auth')) return NextResponse.next()
     const token = await getToken({ req, secret })
     if (token == null) {
-        // Respond with JSON indicating an error message
         return new NextResponse(
             JSON.stringify({
                 success: false,
