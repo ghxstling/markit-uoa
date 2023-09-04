c7d1ad6ef46ba93c76e9fd46949cff2784a657de
HaichuanZ <haichuanzhang.1028@gmail.com>
2023-09-05 10:51:25 +1200

    chore: restructure page

--- src/app/coordinatorDashboard/layout.tsx
+++ src/app/coordinatorDashboard/layout.tsx
@@ -1,17 +1,22 @@
-import { Box } from "@mui/material";
-import Sidebar from "../components/Sidebar";
+import { Box, Stack } from '@mui/material'
+import Sidebar from '../components/Sidebar'
 
 export default function coordinatorDashboardLayout({
     children,
 }: {
-    children: React.ReactNode;
+    children: React.ReactNode
 }) {
     return (
         <html lang="en">
             <body>
-                <Sidebar />
-                {children}
+                <Stack direction={'row'}>
+                    {/* <Box sx={{ width: '15rem' }}> */}
+                    <Box sx={{ width: '15rem' }}>
+                        <Sidebar />
+                    </Box>
+                    <Box sx={{ mt: '50px', ml: '96px' }}>{children}</Box>
+                </Stack>
             </body>
         </html>
-    );
+    )
 }
--- src/app/coordinatorDashboard/page.tsx
+++ src/app/coordinatorDashboard/page.tsx
@@ -1,40 +1,56 @@
-import * as React from "react";
-import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import Divider from "@mui/material/Divider";
-import RecentActivityTable from "../components/RecentActivityTable";
-import Button from "@mui/material/Button";
+'use client'
+
+import * as React from 'react'
+import Typography from '@mui/material/Typography'
+import Stack from '@mui/material/Stack'
+import Divider from '@mui/material/Divider'
+import RecentActivityTable from '../components/RecentActivityTable'
+import Button from '@mui/material/Button'
+import DynamicBreadcrumb from '../components/DynamicBreadcrumb'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import Sidebar from '../components/Sidebar'
+import { Box } from '@mui/material'
 
 export default function coordinatorDashboard() {
+    const { data: session } = useSession()
+
+    let firstName: string = ''
+
+    if (session && session.user && session.user.name && session.user.email) {
+        firstName = session.user.name.slice(
+            0,
+            session.user.name.lastIndexOf(' ') + 1
+        )
+    }
+
     return (
         <>
-            <Stack paddingLeft={"20rem"} paddingRight={"5rem"}>
-                <Typography variant="h6" paddingBottom={"3rem"}>
-                    Breadcrumb Placeholder
+            <Stack sx={{ display: 'inline-block' }}>
+                <DynamicBreadcrumb />
+                <Typography
+                    sx={{ mt: '28px', mb: '53px' }}
+                    variant="h4"
+                    fontWeight="bold"
+                >
+                    Welcome, {firstName}
                 </Typography>
-                <Stack paddingLeft={"3rem"}>
-                    <Typography
-                        variant="h5"
-                        fontWeight={"bold"}
-                        paddingBottom={"3%"}
-                    >
-                        Welcome, Burkhard!
-                    </Typography>
+                <Link href="./" passHref>
                     <Button
                         variant="contained"
-                        style={{
-                            width: "12rem",
-                            backgroundColor: "#00467F",
-                            marginBottom: "5%",
+                        sx={{
+                            backgroundColor: '#00467F',
                         }}
                     >
                         VIEW ALL COURSES
                     </Button>
-                    <Typography variant="h5">Recent Activity</Typography>
-                    <Divider variant="fullWidth" sx={{ mb: "5%" }} />
-                    <RecentActivityTable />
-                </Stack>
+                </Link>
+                <Typography variant="h5" fontWeight="bold" sx={{ mt: '58px' }}>
+                    Recent Activity
+                </Typography>
+                <Divider variant="fullWidth" sx={{ mb: '40px' }} />
+                <RecentActivityTable />
             </Stack>
         </>
-    );
+    )
 }
