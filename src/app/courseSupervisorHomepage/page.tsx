473dcc890228697bf1ee6bf59f4342c210a0fbf8
TheFatCat <74298130+TheFatCat27@users.noreply.github.com>
2023-09-05 17:23:53 +1200

    fixed issue with Pagination

--- src/app/courseSupervisorHomepage/page.tsx
+++ src/app/courseSupervisorHomepage/page.tsx
@@ -20,11 +20,11 @@ import TableRow from '@mui/material/TableRow';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { useSession } from 'next-auth/react';
 import React, { useEffect, useState } from 'react';
 import DynamicBreadcrumb from '../components/DynamicBreadcrumb';
 import Sidebar from "../components/Sidebar";
+import { TableBody } from '@mui/material';
 
 interface TablePaginationActionsProps {
     count: number;
@@ -40,11 +40,11 @@ interface TablePaginationActionsProps {
 export default function CSHomepage(){
 
     interface Course {
-        course: string
+        courseCode: string
         semester: string
-        markers: number
+        markersNeeded: number
         applicants: number
-        status: boolean
+        needMarkers: boolean
         id: number
     }
     
@@ -57,7 +57,7 @@ export default function CSHomepage(){
 
     const fetchData = async () => {
         try {
-          const response = await fetch('https://64edee691f87218271420833.mockapi.io/Courses/Course');
+          const response = await fetch('/api/courses', { method:'GET'});
           const jsonData = await response.json();
           setData(jsonData);
         } catch (error) {
@@ -69,7 +69,7 @@ export default function CSHomepage(){
       const [rowsPerPage, setRowsPerPage] = React.useState(5);
     
       const emptyRows =
-        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
+        page >= 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
     
       const handleChangePage = (
         event: React.MouseEvent<HTMLButtonElement> | null,
@@ -102,18 +102,15 @@ export default function CSHomepage(){
 
 
     return(
-        <div>
-            <div>
-                <Sidebar/>
-            </div>
-            <div style={{
+        <>
+            <Sidebar/>
+            <Box style={{
                 paddingTop: 50,
                 paddingLeft: 350,
-                paddingRight:100
+                paddingRight:100,
+                marginBottom: 50
                 }}>
-                <div style={{display:"inline-flex", fontSize: 16}}>
-                <DynamicBreadcrumb></DynamicBreadcrumb>
-                </div>
+                <DynamicBreadcrumb/>
                 <Typography
                         sx={{ mt: '28px' }}
                         variant="h4"
@@ -121,11 +118,12 @@ export default function CSHomepage(){
                         Welcome, {firstName}
                     </Typography>
                 <Button variant="contained" sx={{backgroundColor: '#00467F', mt: '53px', mb: '58px',}}>CREATE NEW COURSE</Button>
+
                 <TableContainer component={Paper} style={{marginTop:20}}>
                     <Table style={{paddingTop:40}}>
                         <TableHead>
-                            <TableRow style={{width:'100%'}}>
-                                <TableCell style={{textAlign:'center'}}>Course <ArrowDownwardIcon></ArrowDownwardIcon></TableCell>
+                            <TableRow style={{}}>
+                                <TableCell style={{textAlign:'center'}}>Course<ArrowDownwardIcon/></TableCell>
                                 <TableCell style={{textAlign:'center'}}>Semester <ArrowDownwardIcon/></TableCell>
                                 <TableCell style={{textAlign:'center'}}>Edit Course Details</TableCell>
                                 <TableCell style={{textAlign:'center'}}>Markers Needed <Tooltip title="Markers"><InfoOutlinedIcon/></Tooltip> <ArrowDownwardIcon/></TableCell>
@@ -133,54 +131,51 @@ export default function CSHomepage(){
                                 <TableCell style={{textAlign:'center'}}>Status <Tooltip title="status"><InfoOutlinedIcon/></Tooltip> </TableCell>
                             </TableRow>
                         </TableHead>
-                        {(rowsPerPage > 0
-                            ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            : data
-                        ).map((course, index) => (
-                            <TableRow key={index} style={{}}>
-                                <TableCell style={{textAlign:'center'}}>{course.course}</TableCell>
-                                <TableCell style={{textAlign:'center'}}>{course.semester}</TableCell>
-                                <TableCell style={{textAlign:'center'}}><Button>Edit</Button></TableCell>
-                                <TableCell style={{textAlign:'center'}}>{course.markers}</TableCell>
-                                <TableCell style={{textAlign:'center'}}>{course.applicants}</TableCell>
-                                <TableCell style={{textAlign:'center'}}> {course.status ? (
-                                    <Button variant="contained" color="success" style={{width:'75%'}}>
-                                      Complete
-                                    </Button>
-                                  ) : (
-                                    <Button variant="contained" color="error" style={{width:'75%'}}>
-                                      Incomplete
-                                    </Button>
-                                  )}
-                                </TableCell>
+                        <TableBody>
+                          {(rowsPerPage > 0
+                              ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                              : data
+                          ).map((course, index) => (
+                              <TableRow key={index} style={{}}>
+                                  <TableCell style={{textAlign:'center'}}>{course.courseCode}</TableCell>
+                                  <TableCell style={{textAlign:'center'}}>{course.semester}</TableCell>
+                                  <TableCell style={{textAlign:'center'}}><Button href='src/app/dashboard/courses/[courseId]/page.tsx'>Edit</Button></TableCell>
+                                  <TableCell style={{textAlign:'center'}}>{course.markersNeeded}</TableCell>
+                                  <TableCell style={{textAlign:'center'}}>{course.applicants}</TableCell>
+                                  <TableCell style={{textAlign:'center'}}> {course.status ? (
+                                      <Button variant="contained" color="success" style={{width:'75%'}}>
+                                        Complete
+                                      </Button>
+                                    ) : (
+                                      <Button variant="contained" color="error" style={{width:'75%'}}>
+                                        Incomplete
+                                      </Button>
+                                    )}
+                                  </TableCell>
+                              </TableRow>
+                          ))}
+                          {emptyRows > 0 && (
+                            <TableRow style={{ height: 69.5 * emptyRows }}>
+                              <TableCell colSpan={6} />
                             </TableRow>
-                        ))}
-                        {emptyRows > 0 && (
-                          <TableRow style={{ height: 69.5 * emptyRows }}>
-                            <TableCell colSpan={6} />
-                          </TableRow>
-                        )}
-                        <TableFooter style={{ display: 'flex'}}>
-                            <TableRow>
-                              <div style={{ textAlign: 'center' }}>
-                                <TablePagination 
-                                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                                colSpan={3}
-                                count={data.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                onPageChange={handleChangePage}
-                                onRowsPerPageChange={handleChangeRowsPerPage}
-                                showFirstButton= {true}
-                                showLastButton= {true}
-                                />
-                              </div>
-                            </TableRow>
-                        </TableFooter>
+                          )}
+                        </TableBody>
                     </Table>
+                    <TablePagination 
+                      component='div'
+                      sx={{width:'100%'}}
+                      rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                      colSpan={3}
+                      count={data.length}
+                      rowsPerPage={rowsPerPage}
+                      page={page}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                      showFirstButton= {true}
+                      showLastButton= {true}
+                    />
                 </TableContainer>
-                <div style={{paddingTop:50}}></div>
-            </div>
-        </div>
+            </Box>
+        </>
     )
 }
