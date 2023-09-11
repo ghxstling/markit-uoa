90170833b11fb03628def211d46a87b86f2bd4f6
xano <62649990+Zen-ai@users.noreply.github.com>
2023-09-11 18:08:41 +1200

    New file uploading style
    having 2 inputs, each for one file

--- src/app/components/ApplicationForms/CVAndTranscript.tsx
+++ src/app/components/ApplicationForms/CVAndTranscript.tsx
@@ -11,12 +11,12 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props,
 })
 
 const CVAndTranscript = () => {
-    //have a seperate file upload for CV and Transcript
-    const [files, setFiles] = useState<File[]>([])
-    const [fileLimit, setFileLimit] = useState(false)
+    const [cvFileName, setCvFileName] = useState<string>('No file uploaded...')
+    const [transcriptFileName, setTranscriptFileName] = useState<string>('No file uploaded...')
+
     const [openSnackBar, setOpenSnackBar] = useState(false)
 
-    const MAX_COUNT = 2
+    const MAX_COUNT = 1
 
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
@@ -26,45 +26,46 @@ const CVAndTranscript = () => {
         setOpenSnackBar(false)
     }
 
-    const handleFileDelete = (index: number) => {
-        const newFiles = [...files]
-        newFiles.splice(index, 1)
-        setFiles(newFiles)
+    const handleCvFileDelete = () => {
+        setCvFileName('No file uploaded...')
+        ;(document.getElementById('cvFileInput') as HTMLInputElement).value = ''
     }
 
-    const handleFileEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const uploadedFiles: File[] = Array.prototype.slice.call(event.target.files)
-        for (let i = 0; i < uploadedFiles.length; i++) {
-            const file = uploadedFiles[i]
-            if (file.type !== 'application/pdf') {
+    const handleCvFileEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            if (event.target.files[0].type !== 'application/pdf') {
                 setOpenSnackBar(true)
-                event.target.value = '' // clear the input
                 return
             }
-            handleFileUpload(uploadedFiles)
+            setCvFileName(event.target.files[0].name)
+            //handleCvFileUpload(event.target.files[0])
         }
     }
 
-    const handleFileUpload = (uploadedFiles: File[]) => {
-        const uploaded = [...files]
-        let limitExceeded = false
-        uploadedFiles.some((file: File) => {
-            if (uploaded.findIndex((f) => f.name === file.name) === -1) {
-                uploaded.push(file)
-                if (uploaded.length === MAX_COUNT) {
-                    setFileLimit(true)
-                }
-                if (uploaded.length > MAX_COUNT) {
-                    alert(`You can only add a maximum of ${MAX_COUNT} files`)
-                    setFileLimit(false)
-                    limitExceeded = true
-                    return true
-                }
+    const handleCvFileUpload = (uploadedFile: File) => {
+        //post to api
+        return
+    }
+
+    const handleTranscriptFileDelete = () => {
+        setTranscriptFileName('No file uploaded...')
+        ;(document.getElementById('transcriptFileInput') as HTMLInputElement).value = ''
+    }
+
+    const handleTranscriptFileEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            if (event.target.files[0].type !== 'application/pdf') {
+                setOpenSnackBar(true)
+                return
             }
-        })
-        if (!limitExceeded) {
-            setFiles(uploaded)
+            setTranscriptFileName(event.target.files[0].name)
         }
+        //handleTranscriptFileUpload(event.target.files[0])
+    }
+
+    const handleTranscriptFileUpload = (uploadedFile: File) => {
+        //post to api
+        return
     }
 
     return (
@@ -72,68 +73,133 @@ const CVAndTranscript = () => {
             <Typography variant="h5" fontWeight="bold" sx={{ mb: '30px' }}>
                 CV and Academic Transcript Upload
             </Typography>
-            <form
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    border: '2px dashed #1475cf',
-                    height: '300px',
-                    cursor: 'pointer',
-                    borderRadius: '5px',
-                }}
-                onClick={() => (document.querySelector('.input-field') as HTMLElement).click()}
-            >
-                <input
-                    type="file"
-                    multiple
-                    className="input-field"
-                    accept="application/pdf"
-                    hidden
-                    onChange={handleFileEvent}
-                />
 
-                <CloudUploadIcon style={{ color: '#1475cf', fontSize: '60px' }} />
-                <Typography variant="subtitle1">Click to upload</Typography>
-                <Typography variant="subtitle2">PDF only</Typography>
-                <Typography variant="subtitle2">MAXIMUM 2 FILES</Typography>
-            </form>
-
-            {/* Map each file to its own grid item */}
-            {files.map((file: File, index: number) => (
-                <>
-                    <Grid container spacing={1} width={809}>
+            <Grid container spacing={5} direction="row" width="100%" justifyContent="space-between">
+                <Grid item width="50%">
+                    <Grid container width="100%">
                         <Grid item width="100%">
-                            <Grid
-                                spacing={1}
-                                container
-                                alignItems="center"
-                                justifyContent="space-between"
-                                sx={{ m: '10px 0px', backgroundColor: '#e9f0fe', p: '1px 2px' }}
-                                width="100%"
-
-                                //margin: '10px 0',
-                                //padding: '15px 20px'
+                            <form
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    border: '2px dashed #1475cf',
+                                    height: '300px',
+                                    cursor: 'pointer',
+                                    borderRadius: '5px',
+                                }}
+                                onClick={() => (document.querySelector('.input-field-cv') as HTMLElement).click()}
                             >
-                                <Grid item>
-                                    <Grid container alignItems="center" spacing={2}>
-                                        <Grid item>
-                                            <DescriptionIcon style={{ color: '#1475cf' }} />
+                                <input
+                                    id="cvFileInput"
+                                    type="file"
+                                    className="input-field-cv"
+                                    accept="application/pdf"
+                                    hidden
+                                    onChange={handleCvFileEvent}
+                                />
+                                <CloudUploadIcon style={{ color: '#1475cf', fontSize: '60px' }} />
+                                <Typography variant="subtitle1">Click to upload CV</Typography>
+                                <Typography variant="subtitle2">PDF only</Typography>
+                                <Typography variant="subtitle2">MAXIMUM 1 FILE</Typography>
+                            </form>
+                            <Grid item>
+                                <Grid container spacing={1} width="100%">
+                                    <Grid item width="100%">
+                                        <Grid
+                                            spacing={1}
+                                            container
+                                            alignItems="center"
+                                            justifyContent="space-between"
+                                            sx={{ m: '10px 0px', backgroundColor: '#e9f0fe', p: '1px 2px' }}
+                                            width="100%"
+                                        >
+                                            <Grid item>
+                                                <Grid container alignItems="center" spacing={2}>
+                                                    <Grid item>
+                                                        <DescriptionIcon style={{ color: '#1475cf' }} />
+                                                    </Grid>
+                                                    <Grid item>{cvFileName}</Grid>
+                                                </Grid>
+                                            </Grid>
+                                            <Grid item>
+                                                <Button onClick={handleCvFileDelete}>
+                                                    <DeleteIcon style={{ color: '#1475cf' }} />
+                                                </Button>
+                                            </Grid>
                                         </Grid>
-                                        <Grid item>{file.name}</Grid>
                                     </Grid>
-                                </Grid>
-                                <Grid item>
-                                    <Button onClick={() => handleFileDelete(index)}>
-                                        <DeleteIcon style={{ color: '#1475cf' }} />
-                                    </Button>
                                 </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
-                </>
-            ))}
+                </Grid>
+
+                <Grid item width="50%">
+                    <Grid container width="100%">
+                        <Grid item width="100%">
+                            <form
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    border: '2px dashed #1475cf',
+                                    height: '300px',
+                                    cursor: 'pointer',
+                                    borderRadius: '5px',
+                                }}
+                                onClick={() =>
+                                    (document.querySelector('.input-field-transcript') as HTMLElement).click()
+                                }
+                            >
+                                <input
+                                    id="transcriptFileInput"
+                                    type="file"
+                                    className="input-field-transcript"
+                                    accept="application/pdf"
+                                    hidden
+                                    onChange={handleTranscriptFileEvent}
+                                />
+                                <CloudUploadIcon style={{ color: '#1475cf', fontSize: '60px' }} />
+                                <Typography variant="subtitle1">Click to upload Transcript</Typography>
+                                <Typography variant="subtitle2">PDF only</Typography>
+                                <Typography variant="subtitle2">MAXIMUM 1 FILE</Typography>
+                            </form>
+                            <Grid item>
+                                <Grid container spacing={1} width="100%">
+                                    <Grid item width="100%">
+                                        <Grid
+                                            spacing={1}
+                                            container
+                                            alignItems="center"
+                                            justifyContent="space-between"
+                                            sx={{ m: '10px 0px', backgroundColor: '#e9f0fe', p: '1px 2px' }}
+                                            width="100%"
+                                        >
+                                            <Grid item>
+                                                <Grid container alignItems="center" spacing={2}>
+                                                    <Grid item>
+                                                        <DescriptionIcon style={{ color: '#1475cf' }} />
+                                                    </Grid>
+                                                    <Grid item>{transcriptFileName}</Grid>
+                                                </Grid>
+                                            </Grid>
+                                            <Grid item>
+                                                <Button onClick={handleTranscriptFileDelete}>
+                                                    <DeleteIcon style={{ color: '#1475cf' }} />
+                                                </Button>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+
             <Snackbar
                 anchorOrigin={{
                     vertical: 'bottom',
