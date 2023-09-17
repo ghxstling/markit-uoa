b9ef6819fbce2f4e77fd851f5110a350570924f7
HaichuanZ <haichuanzhang.1028@gmail.com>
2023-09-18 08:15:22 +1200

    chore: import updates

--- src/app/dashboard/@supervisor/page.tsx
+++ src/app/dashboard/@supervisor/page.tsx
@@ -4,8 +4,7 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import { useSession } from 'next-auth/react'
-import Sidebar from '../../components/Sidebar'
-import CourseTable from '../../components/courses/CourseTable'
+import CourseTable from '@/app/components/courses/CourseTable'
 import React from 'react'
 
 export default function CSHomepage() {
