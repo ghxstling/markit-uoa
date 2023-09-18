47543b61047237592480babb88dac1ac6f920634
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2023-09-18 12:55:12 +1200

    refactor: move test helper functions

--- src/helpers/testHelper.ts
+++ src/helpers/testHelper.ts
@@ -1,6 +1,24 @@
-import prisma from "@/libs/prisma";
+import prisma from '@/libs/prisma'
+import { Prisma } from '@prisma/client'
 
 export const resetDatabase = async () => {
-    const deleteCourse = prisma.course.deleteMany();
-    await prisma.$transaction([deleteCourse]);
-};
+    const deleteCourse = prisma.course.deleteMany()
+    const deleteApplication = prisma.application.deleteMany()
+    const deleteStudent = prisma.student.deleteMany()
+    const deleteUser = prisma.user.deleteMany()
+    await prisma.$transaction([deleteCourse, deleteApplication, deleteStudent, deleteUser])
+}
+
+export const courseInputHelper = (courseCode: string, courseDescription: string): Prisma.CourseCreateInput => {
+    return {
+        courseCode,
+        courseDescription,
+        numOfEstimatedStudents: 5,
+        numOfEnrolledStudents: 6,
+        markerHours: 7,
+        needMarkers: true,
+        markersNeeded: 9,
+        semester: '2022S1',
+        markerResponsibilities: 'Marker responsibilities',
+    }
+}
