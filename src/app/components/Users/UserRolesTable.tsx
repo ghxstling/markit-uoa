712546f5ae6c6d3ff1b1b9f55752894e99259ffb
Alex Reid <arei209@aucklanduni.ac.nz>
2023-09-19 17:41:33 +1200

    Adding search functionality and filtering

--- src/app/components/Users/UserRolesTable.tsx
+++ src/app/components/Users/UserRolesTable.tsx
@@ -11,6 +11,10 @@ import {
     MenuItem,
     SelectChangeEvent,
     TableFooter,
+    Container,
+    Typography,
+    TextField,
+    Box,
 } from '@mui/material'
 import { Role } from '@/models/role'
 import React, { useEffect, useState } from 'react'
@@ -26,6 +30,9 @@ export default function UserRolesTable() {
     const [users, setUsers] = useState<User[]>([])
     const [page, setPage] = useState(0)
     const [rowsPerPage, setRowsPerPage] = useState(5)
+    const [sortConfig, setSortConfig] = useState<{ key: keyof User; direction: 'ascending' | 'descending' } | null>(
+        null
+    )
 
     const isClient = typeof window !== 'undefined'
 
@@ -82,55 +89,125 @@ export default function UserRolesTable() {
         }
     }
 
+    function handleSort(column: keyof User) {
+        let direction: 'ascending' | 'descending' = 'ascending'
+        if (sortConfig && sortConfig.key === column && sortConfig.direction === 'ascending') {
+            direction = 'descending'
+        }
+        setSortConfig({ key: column, direction })
+    }
+
+    const sortedUsers = [...users].sort((a, b) => {
+        if (sortConfig === null) return 0
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? -1 : 1
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? 1 : -1
+        }
+        return 0
+    })
+
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value)
+    }
+
+    const filteredUsers = sortedUsers.filter(
+        (user) =>
+            user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.role.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
     return (
-        <TableContainer component={Paper} style={{ marginTop: 20 }}>
-            <Table style={{ paddingTop: 40 }}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell style={{ textAlign: 'center' }}>Name</TableCell>
-                        <TableCell style={{ textAlign: 'center' }}>Email</TableCell>
-                        <TableCell style={{ textAlign: 'center' }}>Role</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {(rowsPerPage > 0 ? users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : users).map(
-                        (user) => (
-                            <TableRow key={user.id}>
-                                <TableCell style={{ textAlign: 'center' }}>{user.name}</TableCell>
-                                <TableCell style={{ textAlign: 'center' }}>{user.email}</TableCell>
-                                <TableCell style={{ textAlign: 'center' }}>
-                                    <Select
-                                        value={user.role}
-                                        onChange={(event) => handleRoleChange(event, user.id)}
-                                        displayEmpty
-                                    >
-                                        <MenuItem value={Role.Student}>Student</MenuItem>
-                                        <MenuItem value={Role.Supervisor}>Supervisor</MenuItem>
-                                        <MenuItem value={Role.Coordinator}>Coordinator</MenuItem>
-                                    </Select>
+        <Container style={{ marginTop: 20 }}>
+            <Paper elevation={3} style={{ padding: '20px' }}>
+                <Box display="flex" justifyContent="space-between" alignItems="center">
+                    <Typography variant="h4" fontWeight={600} style={{ marginBottom: '20px', fontSize: '1.8rem' }}>
+                        Manage User Roles
+                    </Typography>
+                    <TextField
+                        variant="outlined"
+                        label="Search"
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                        style={{ marginBottom: 20 }}
+                    />
+                </Box>
+                <TableContainer component={Paper} style={{ marginTop: 20 }}>
+                    <Table style={{ paddingTop: 40 }}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell
+                                    style={{
+                                        textAlign: 'center',
+                                        minWidth: '250px',
+                                        fontWeight: 'bold',
+                                        cursor: 'pointer',
+                                    }}
+                                    onClick={() => handleSort('name')}
+                                >
+                                    Name
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        textAlign: 'center',
+                                        minWidth: '250px',
+                                        fontWeight: 'bold',
+                                        cursor: 'pointer',
+                                    }}
+                                    onClick={() => handleSort('email')}
+                                >
+                                    Email
+                                </TableCell>
+                                <TableCell style={{ textAlign: 'center', minWidth: '250px', fontWeight: 'bold' }}>
+                                    Role
                                 </TableCell>
                             </TableRow>
-                        )
-                    )}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TableCell colSpan={3}>
-                            <TablePagination
-                                component="div" // ensures TablePagination renders as a div, not a TableCell
-                                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                                count={users.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                onPageChange={handleChangePage}
-                                onRowsPerPageChange={handleChangeRowsPerPage}
-                                showFirstButton={true}
-                                showLastButton={true}
-                            />
-                        </TableCell>
-                    </TableRow>
-                </TableFooter>
-            </Table>
-        </TableContainer>
+                        </TableHead>
+                        <TableBody>
+                            {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((user) => (
+                                <TableRow key={user.id}>
+                                    <TableCell style={{ textAlign: 'center' }}>{user.name}</TableCell>
+                                    <TableCell style={{ textAlign: 'center' }}>{user.email}</TableCell>
+                                    <TableCell style={{ textAlign: 'center', width: '150px' }}>
+                                        <Select
+                                            value={user.role}
+                                            onChange={(event) => handleRoleChange(event, user.id)}
+                                            displayEmpty
+                                            style={{ width: '100%', fontSize: '0.8rem' }}
+                                            size="small"
+                                        >
+                                            <MenuItem value={Role.Student}>Student</MenuItem>
+                                            <MenuItem value={Role.Supervisor}>Supervisor</MenuItem>
+                                            <MenuItem value={Role.Coordinator}>Coordinator</MenuItem>
+                                        </Select>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TableCell colSpan={3}>
+                                    <TablePagination
+                                        component="div"
+                                        rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                        count={filteredUsers.length}
+                                        rowsPerPage={rowsPerPage}
+                                        page={page}
+                                        onPageChange={handleChangePage}
+                                        onRowsPerPageChange={handleChangeRowsPerPage}
+                                        showFirstButton={true}
+                                        showLastButton={true}
+                                    />
+                                </TableCell>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </Container>
     )
 }
