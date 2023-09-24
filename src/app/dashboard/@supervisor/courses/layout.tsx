bbb6d704a0591744675e245d6c220e1fc8b4670a
HaichuanZ <haichuanzhang.1028@gmail.com>
2023-09-24 15:00:08 +1300

    feat: supervisor courses responsive fix

--- src/app/dashboard/@supervisor/courses/layout.tsx
+++ src/app/dashboard/@supervisor/courses/layout.tsx
@@ -2,20 +2,22 @@ import { Box, Stack } from '@mui/material'
 import Sidebar from '@/app/components/Sidebar'
 import DynamicBreadcrumb from '@/app/components/DynamicBreadcrumb'
 
-export default function supervisorCreateCourseLayout({ children }: { children: React.ReactNode }) {
+export default function SupervisorCreateCourseLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body>
-                <Stack direction={'row'}>
-                    <Box sx={{ width: '15rem' }}>
-                        <Sidebar />
+                <Stack>
+                    <Box
+                        sx={{
+                            mt: '60px',
+                            ml: { sm: '300px', md: '300px', lg: '300px', xl: '350px' },
+                        }}
+                    >
+                        <DynamicBreadcrumb />
                     </Box>
-                    <Stack>
-                        <Box sx={{ mt: '60px', ml: '400px' }}>
-                            <DynamicBreadcrumb />
-                        </Box>
-                        <Box sx={{ mt: '25px', ml: '500px' }}>{children}</Box>
-                    </Stack>
+                    <Box sx={{ mt: '25px', ml: { sm: '300px', md: '200px', lg: '200px', xl: '250px' } }}>
+                        {children}
+                    </Box>
                 </Stack>
             </body>
         </html>
