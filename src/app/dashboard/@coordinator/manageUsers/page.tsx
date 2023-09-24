052fa4e8dbca20a7b2534c4f46219e4bad2635d2
HaichuanZ <haichuanzhang.1028@gmail.com>
2023-09-25 10:37:27 +1300

    style: responsive coordinator manageUsers

--- src/app/dashboard/@coordinator/manageUsers/page.tsx
+++ src/app/dashboard/@coordinator/manageUsers/page.tsx
@@ -2,11 +2,31 @@
 import Sidebar from '../../../components/Sidebar'
 import DynamicBreadcrumb from '../../../components/DynamicBreadcrumb'
 import UserRolesTable from '../../../components/Users/UserRolesTable'
+import { Box, Stack } from '@mui/material'
+import CustomTheme from '@/app/CustomTheme'
+import { ThemeProvider } from '@mui/material/styles'
 
 export default function ManageUsersPage() {
     return (
-        <>
-            <UserRolesTable />
-        </>
+        <ThemeProvider theme={CustomTheme}>
+            <Stack>
+                <Box
+                    sx={{
+                        mt: '100px',
+                        ml: { sm: '50px', md: '100px', lg: '150px', xl: '200px', xxl: '250px', xxxl: '300px' },
+                    }}
+                >
+                    <DynamicBreadcrumb />
+                </Box>
+                <Box
+                    sx={{
+                        mt: '25px',
+                        ml: { sm: '50px', md: '100px', lg: '150px', xl: '200px', xxl: '250px', xxxl: '300px' },
+                    }}
+                >
+                    <UserRolesTable />
+                </Box>
+            </Stack>
+        </ThemeProvider>
     )
 }
