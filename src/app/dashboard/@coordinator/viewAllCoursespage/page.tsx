4573a728fc776b430142ea4f331bf1cfc527d8f1
HaichuanZ <haichuanzhang.1028@gmail.com>
2023-09-25 10:33:57 +1300

    style: responsive coordinator viewAllCourses

--- src/app/dashboard/@coordinator/viewAllCoursespage/page.tsx
+++ src/app/dashboard/@coordinator/viewAllCoursespage/page.tsx
@@ -1,18 +1,35 @@
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
+'use client'
 import Typography from '@mui/material/Typography'
 import { useSession } from 'next-auth/react'
 import DynamicBreadcrumb from '@/app/components/DynamicBreadcrumb'
 import Sidebar from '@/app/components/Sidebar'
 import CourseTable from '@/app/components/courses/CourseTable'
+import { Box, Stack } from '@mui/material'
+import CustomTheme from '@/app/CustomTheme'
+import { ThemeProvider } from '@mui/material/styles'
 
 export default function AllCourseView() {
     return (
-        <>
-            <Box>
-                <h2>Course View</h2>
-                <CourseTable />
-            </Box>
-        </>
+        <ThemeProvider theme={CustomTheme}>
+            <Stack>
+                <Box
+                    sx={{
+                        mt: '100px',
+                        ml: { sm: '50px', md: '100px', lg: '150px', xl: '200px', xxl: '250px', xxxl: '300px' },
+                    }}
+                >
+                    <DynamicBreadcrumb />
+                </Box>
+                <Box
+                    sx={{
+                        mt: '25px',
+                        ml: { sm: '50px', md: '100px', lg: '150px', xl: '200px', xxl: '250px', xxxl: '300px' },
+                    }}
+                >
+                    <h2>Course View</h2>
+                    <CourseTable />
+                </Box>
+            </Stack>
+        </ThemeProvider>
     )
 }
