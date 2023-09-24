03ca5d4ec02e5b373fc9e566562029090a5adbd3
HaichuanZ <haichuanzhang.1028@gmail.com>
2023-09-24 15:02:07 +1300

    refactor: supervisor courses/id naming

--- src/app/dashboard/@supervisor/courses/[courseId]/layout.tsx
+++ src/app/dashboard/@supervisor/courses/[courseId]/layout.tsx
@@ -2,6 +2,6 @@ import { Box, Stack } from '@mui/material'
 import Sidebar from '@/app/components/Sidebar'
 import DynamicBreadcrumb from '@/app/components/DynamicBreadcrumb'
 
-export default function supervisorEditCourseLayout({ children }: { children: React.ReactNode }) {
+export default function SupervisorEditCourseLayout({ children }: { children: React.ReactNode }) {
     return <>{children}</>
 }
