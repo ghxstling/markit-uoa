056686987077533b63168ce108b82e4cb6ff0964
HaichuanZ <haichuanzhang.1028@gmail.com>
2023-09-25 22:14:57 +1300

    Style: actually responsive student view

--- src/app/dashboard/@student/Application/page.tsx
+++ src/app/dashboard/@student/Application/page.tsx
@@ -11,6 +11,7 @@ import MuiAlert, { AlertProps } from '@mui/material/Alert'
 import CoursePreferences from '@/app/components/ApplicationForms/CoursePreferences'
 import CustomTheme from '@/app/CustomTheme'
 import { ThemeProvider } from '@mui/material/styles'
+import Sidebar from '@/app/components/Sidebar'
 
 const steps = ['Personal Details', 'Employment Details', 'CV and Academic Transcript Upload', 'Course Preferences']
 
@@ -126,84 +127,96 @@ const Application = () => {
 
     return (
         <ThemeProvider theme={CustomTheme}>
+            <Sidebar />
             <Box
                 sx={{
+                    height: '100vh',
                     display: 'flex',
-                    mt: '40px',
-                    ml: { sm: '50px', md: '100px', lg: '200px', xl: '250px', xxl: '300px', xxxl: '350px' },
+                    flexDirection: 'column',
+                    justifyContent: 'safe center',
+                    ml: '240px',
                 }}
             >
-                {/* Create basic layout */}
-                <Container component="main" maxWidth="md" sx={{ mb: 4 }}>
-                    <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-                        <Typography component="h1" variant="h4" align="center">
-                            Application
-                        </Typography>
-                        <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-                            {steps.map((label) => (
-                                <Step key={label}>
-                                    <StepLabel>{label}</StepLabel>
-                                </Step>
-                            ))}
-                        </Stepper>
-                        {activeStep === steps.length ? (
-                            <>
-                                <Typography variant="h5" gutterBottom>
-                                    Thank you for your application.
-                                </Typography>
-                            </>
-                        ) : (
-                            <>
-                                {getStepContent(activeStep, formValues, setFormValues)}
-                                <Box
-                                    sx={{
-                                        display: 'flex',
-                                        justifyContent: 'flex-end',
-                                    }}
-                                >
-                                    {activeStep !== 0 && (
-                                        <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                                            Back
-                                        </Button>
-                                    )}
-                                    {activeStep === steps.length - 1 ? (
-                                        formValues.coursePreferences.length === 0 ? (
-                                            <Button
-                                                variant="contained"
-                                                disabled
-                                                onClick={handleNext}
-                                                sx={{ mt: 3, ml: 1 }}
-                                            >
-                                                Submit Application
+                <Box
+                    sx={{
+                        mt: '20px',
+                        ml: { sm: '60px', lg: '120px' },
+                        mr: { sm: '60px', lg: '120px' },
+                        mb: '20px',
+                    }}
+                >
+                    {/* Create basic layout */}
+                    <Container component="main" maxWidth="md" sx={{ mb: 4 }}>
+                        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+                            <Typography component="h1" variant="h4" align="center">
+                                Application
+                            </Typography>
+                            <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+                                {steps.map((label) => (
+                                    <Step key={label}>
+                                        <StepLabel>{label}</StepLabel>
+                                    </Step>
+                                ))}
+                            </Stepper>
+                            {activeStep === steps.length ? (
+                                <>
+                                    <Typography variant="h5" gutterBottom>
+                                        Thank you for your application.
+                                    </Typography>
+                                </>
+                            ) : (
+                                <>
+                                    {getStepContent(activeStep, formValues, setFormValues)}
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            justifyContent: 'flex-end',
+                                        }}
+                                    >
+                                        {activeStep !== 0 && (
+                                            <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                                                Back
                                             </Button>
+                                        )}
+                                        {activeStep === steps.length - 1 ? (
+                                            formValues.coursePreferences.length === 0 ? (
+                                                <Button
+                                                    variant="contained"
+                                                    disabled
+                                                    onClick={handleNext}
+                                                    sx={{ mt: 3, ml: 1 }}
+                                                >
+                                                    Submit Application
+                                                </Button>
+                                            ) : (
+                                                <Button variant="contained" onClick={handleNext} sx={{ mt: 3, ml: 1 }}>
+                                                    Submit Application
+                                                </Button>
+                                            )
                                         ) : (
                                             <Button variant="contained" onClick={handleNext} sx={{ mt: 3, ml: 1 }}>
-                                                Submit Application
+                                                Next
                                             </Button>
-                                        )
-                                    ) : (
-                                        <Button variant="contained" onClick={handleNext} sx={{ mt: 3, ml: 1 }}>
-                                            Next
-                                        </Button>
-                                    )}
-                                </Box>
-                                <Snackbar
-                                    anchorOrigin={{
-                                        vertical: 'bottom',
-                                        horizontal: 'right',
-                                    }}
-                                    open={openSnackBar}
-                                    autoHideDuration={6000}
-                                    onClose={handleClose}
-                                >
-                                    <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
-                                        {snackbarMessage}
-                                    </Alert>
-                                </Snackbar>
-                            </>
-                        )}
-                    </Paper>
-                </Container>
+                                        )}
+                                    </Box>
+                                    <Snackbar
+                                        anchorOrigin={{
+                                            vertical: 'bottom',
+                                            horizontal: 'right',
+                                        }}
+                                        open={openSnackBar}
+                                        autoHideDuration={6000}
+                                        onClose={handleClose}
+                                    >
+                                        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                                            {snackbarMessage}
+                                        </Alert>
+                                    </Snackbar>
+                                </>
+                            )}
+                        </Paper>
+                    </Container>
+                </Box>
             </Box>
         </ThemeProvider>
     )
