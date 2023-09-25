d246e497bde1b695a770f9d7d8d3ca2e73401d80
TheFatCat <74298130+TheFatCat27@users.noreply.github.com>
2023-09-25 13:39:14 +1300

    fixed some stuff up

--- src/app/dashboard/@coordinator/[studentId]/page.tsx
+++ src/app/dashboard/@coordinator/[studentId]/page.tsx
@@ -1,5 +1,11 @@
+'use client'
+
+import { DegreeType } from '@/models/degreeType';
+import { Box, Divider } from '@mui/material';
 import { useRouter } from 'next/router';
-import { number } from 'zod';
+import { useEffect, useState } from 'react';
+import { TabContainer } from 'react-bootstrap';
+
 
 export default function viewStudentInformation(){
 
@@ -19,7 +25,53 @@ export default function viewStudentInformation(){
     const router = useRouter();
     const { studentId } = router.query;
 
+    const [data, setData] = useState<Student | null>(null);
+
+    useEffect(() => {
+        if (studentId) {
+          fetchData(studentId as string);
+        }
+      }, [studentId]);
+
+      const fetchData = async (studentId: string) => {
+        try {
+          const response = await fetch(`/api/users/${studentId}`, { method: 'GET' });
+          if (response.ok) {
+            const jsonData = await response.json();
+            setData(jsonData);
+          } else {
+            console.error('Error fetching data:', response.status);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
     return(
-        
-    )
-}
+        <>
+            <Box width={'80%'}>
+                <h1>{data?.upi}-{data?.auid}</h1>
+                <h2>Student Info</h2>
+                <Divider/>
+                <p>Email: {data?.email}</p>
+                <Divider/>
+                {data?.overseas ? (
+                <p>Living Overseas: Yes</p>
+                ) : (
+                <p>Living Overseas: No</p>
+                )}
+                <Divider/>
+                {data?.validWorkVisa ? (
+                <p>Valid Work Visa: Yes</p>
+                ) : (
+                <p>Valid Work Visa: No</p>
+                )}
+                <p>Years into Degree: {data?.degreeYear}</p>
+                <Divider/>
+                <p>Maximum Hours Per Week: {data?.maxWorkHours}</p>
+                <Divider/>
+                <p>Degree Type: {data?.degreeType}</p>
+                <Divider/>
+            </Box>
+        </>     
+)}
