341fc42b1c8647dda57525309a6e6c232bf8bec8
TheFatCat <74298130+TheFatCat27@users.noreply.github.com>
2023-09-25 15:19:34 +1300

    added some styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -24,6 +24,7 @@ export default function Page404(){
                     left: 0,
                     width: '100%',
                     height: '100%',
+                    objectFit: 'cover',
                 }}
             />
         </div>
