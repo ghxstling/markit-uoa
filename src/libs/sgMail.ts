a139064d6febc27773212a490b5fefa43fbd2973
ghxstling <dcho282@aucklanduni.ac.nz>
2023-09-25 14:38:56 +1300

    refactor: moved sgMail client to emailService

--- src/libs/sgMail.ts
+++
@@ -1,6 +0,0 @@
-import sgMail from '@sendgrid/mail'
-const key = process.env.SENDGRID_API_KEY
-if (!key) throw new Error('Missing process.env.SENDGRID_API_KEY')
-
-sgMail.setApiKey(key)
-export default sgMail
