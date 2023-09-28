fc89b40a2b2e3e32eaac97f3e60c50ac934d54be
HaichuanZ <haichuanzhang.1028@gmail.com>
2023-09-29 10:12:32 +1300

    style: responsive fixes

--- src/app/dashboard/@student/ViewAllCourses/page.tsx
+++ src/app/dashboard/@student/ViewAllCourses/page.tsx
@@ -76,154 +76,147 @@ export default function StudentViewAllCourses() {
     return (
         <ThemeProvider theme={CustomTheme}>
             <Sidebar />
-            <Box sx={{ display: 'flex' }}>
-                <Sidebar />
+
+            <Sidebar />
+            <Box
+                sx={{
+                    height: '100vh',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'safe center',
+                    ml: '240px',
+                }}
+            >
                 <Box
                     sx={{
-                        height: '100vh',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'safe center',
-                        ml: '240px',
+                        mt: '20px',
+                        ml: { xs: '5px', lg: '100px', xl: '200px' },
                     }}
                 >
-                    <Box
-                        sx={{
-                            mt: '20px',
-                            ml: { xs: '5px', lg: '100px', xl: '200px' },
-                        }}
-                    >
-                        <DynamicBreadcrumb />
-                    </Box>
-                    <Box
-                        sx={{
-                            mt: '50px',
-                            ml: { xs: '5px', lg: '150px', xl: '250px' },
-                            mr: { xs: '5px', lg: '150px', xl: '250px' },
-                            mb: '100px',
-                        }}
-                    >
-                        <h2>Course View</h2>
-                        <TableContainer component={Paper} style={{ marginTop: 20 }} sx={{ width: '100%' }}>
-                            <Table style={{ paddingTop: 40 }}>
-                                <TableHead>
-                                    <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-                                        <TableCell />
-                                        <TableCell style={{ textAlign: 'center' }}>
-                                            <div style={{ alignItems: 'center', flexWrap: 'wrap' }}>
-                                                Course
-                                                {/*TODO Sort feature<ArrowDownwardIcon style={{marginLeft:5, verticalAlign:"middle"}}/>*/}
-                                            </div>
-                                        </TableCell>
-                                        <TableCell style={{ textAlign: 'center' }}>
-                                            <div style={{ alignItems: 'center', flexWrap: 'wrap' }}>
-                                                Semester{' '}
-                                                {/*TODO Sort feature<ArrowDownwardIcon style={{marginLeft:5, verticalAlign:"middle"}}/>*/}
-                                            </div>
-                                        </TableCell>
-                                        <TableCell style={{ textAlign: 'center' }}>
-                                            <div style={{ alignItems: 'center', flexWrap: 'wrap' }}>
-                                                Markers Needed{' '}
-                                                <Tooltip title="Markers">
-                                                    <InfoOutlinedIcon
-                                                        style={{ marginLeft: 5, verticalAlign: 'middle' }}
-                                                    />
-                                                </Tooltip>{' '}
-                                                {/*TODO Sort feature<ArrowDownwardIcon style={{marginLeft:5, verticalAlign:"middle"}}/>*/}
-                                            </div>
-                                        </TableCell>
-                                        <TableCell style={{ textAlign: 'center' }}>
-                                            <div style={{ alignItems: 'center', flexWrap: 'wrap' }}>
-                                                PlaceHolder{' '}
-                                                <Tooltip title="status">
-                                                    <InfoOutlinedIcon
-                                                        style={{ marginLeft: 5, verticalAlign: 'middle' }}
-                                                    />
-                                                </Tooltip>
-                                            </div>
-                                        </TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {(rowsPerPage > 0
-                                        ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                        : data
-                                    ).map((course, index) => (
-                                        <>
-                                            <TableRow key={index}>
-                                                <TableCell>
-                                                    <IconButton
-                                                        aria-label="expand row"
-                                                        size="small"
-                                                        onClick={() => toggleRow(index)}
-                                                    >
-                                                        {openRows[index] ? (
-                                                            <KeyboardArrowUpIcon />
-                                                        ) : (
-                                                            <KeyboardArrowDownIcon />
-                                                        )}
-                                                    </IconButton>
-                                                </TableCell>
-                                                <TableCell style={{ textAlign: 'center' }}>
-                                                    {course.courseCode}
-                                                </TableCell>
-                                                <TableCell style={{ textAlign: 'center' }}>{course.semester}</TableCell>
-                                                <TableCell style={{ textAlign: 'center' }}>
-                                                    {course.markersNeeded}
-                                                </TableCell>
-                                                <TableCell style={{ textAlign: 'center' }}>PLACEHOLDER</TableCell>
-                                            </TableRow>
-                                            <TableRow>
-                                                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-                                                    <Collapse in={openRows[index]} timeout="auto" unmountOnExit>
-                                                        <Box sx={{ margin: 1 }}>
-                                                            <Typography
-                                                                variant="h6"
-                                                                gutterBottom
-                                                                component="div"
-                                                                sx={{ whiteSpace: 'pre-line' }}
-                                                            >
-                                                                Details:
-                                                            </Typography>
-                                                            <Typography variant="body1">
-                                                                Course Description: {course.courseDescription}
-                                                            </Typography>{' '}
-                                                            <br />
-                                                            <Typography variant="body1">
-                                                                Marker Responsibilities: {course.markerResponsibilities}
-                                                            </Typography>{' '}
-                                                            <br />
-                                                            <Typography variant="body1">
-                                                                Marker Hours: {course.markerHours}
-                                                            </Typography>
-                                                        </Box>
-                                                    </Collapse>
-                                                </TableCell>
-                                            </TableRow>
-                                        </>
-                                    ))}
-                                    {emptyRows > 0 && (
-                                        <TableRow style={{ height: 69.5 * emptyRows }}>
-                                            <TableCell colSpan={6} />
+                    <DynamicBreadcrumb />
+                </Box>
+                <Box
+                    sx={{
+                        mt: '50px',
+                        ml: { xs: '5px', lg: '150px', xl: '250px' },
+                        mr: { xs: '5px', lg: '150px', xl: '250px' },
+                        mb: '100px',
+                    }}
+                >
+                    <h2>Course View</h2>
+                    <TableContainer component={Paper} style={{ marginTop: 20 }}>
+                        <Table style={{ paddingTop: 40 }}>
+                            <TableHead>
+                                <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                                    <TableCell />
+                                    <TableCell style={{ textAlign: 'center' }}>
+                                        <div style={{ alignItems: 'center', flexWrap: 'wrap' }}>
+                                            Course
+                                            {/*TODO Sort feature<ArrowDownwardIcon style={{marginLeft:5, verticalAlign:"middle"}}/>*/}
+                                        </div>
+                                    </TableCell>
+                                    <TableCell style={{ textAlign: 'center' }}>
+                                        <div style={{ alignItems: 'center', flexWrap: 'wrap' }}>
+                                            Semester{' '}
+                                            {/*TODO Sort feature<ArrowDownwardIcon style={{marginLeft:5, verticalAlign:"middle"}}/>*/}
+                                        </div>
+                                    </TableCell>
+                                    <TableCell style={{ textAlign: 'center' }}>
+                                        <div style={{ alignItems: 'center', flexWrap: 'wrap' }}>
+                                            Markers Needed{' '}
+                                            <Tooltip title="Markers">
+                                                <InfoOutlinedIcon style={{ marginLeft: 5, verticalAlign: 'middle' }} />
+                                            </Tooltip>{' '}
+                                            {/*TODO Sort feature<ArrowDownwardIcon style={{marginLeft:5, verticalAlign:"middle"}}/>*/}
+                                        </div>
+                                    </TableCell>
+                                    <TableCell style={{ textAlign: 'center' }}>
+                                        <div style={{ alignItems: 'center', flexWrap: 'wrap' }}>
+                                            PlaceHolder{' '}
+                                            <Tooltip title="status">
+                                                <InfoOutlinedIcon style={{ marginLeft: 5, verticalAlign: 'middle' }} />
+                                            </Tooltip>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {(rowsPerPage > 0
+                                    ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    : data
+                                ).map((course, index) => (
+                                    <>
+                                        <TableRow key={index}>
+                                            <TableCell>
+                                                <IconButton
+                                                    aria-label="expand row"
+                                                    size="small"
+                                                    onClick={() => toggleRow(index)}
+                                                >
+                                                    {openRows[index] ? (
+                                                        <KeyboardArrowUpIcon />
+                                                    ) : (
+                                                        <KeyboardArrowDownIcon />
+                                                    )}
+                                                </IconButton>
+                                            </TableCell>
+                                            <TableCell style={{ textAlign: 'center' }}>{course.courseCode}</TableCell>
+                                            <TableCell style={{ textAlign: 'center' }}>{course.semester}</TableCell>
+                                            <TableCell style={{ textAlign: 'center' }}>
+                                                {course.markersNeeded}
+                                            </TableCell>
+                                            <TableCell style={{ textAlign: 'center' }}>PLACEHOLDER</TableCell>
                                         </TableRow>
-                                    )}
-                                </TableBody>
-                            </Table>
-                            <TablePagination
-                                component="div"
-                                sx={{ width: '100%' }}
-                                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                                colSpan={3}
-                                count={data.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                onPageChange={handleChangePage}
-                                onRowsPerPageChange={handleChangeRowsPerPage}
-                                showFirstButton={true}
-                                showLastButton={true}
-                            />
-                        </TableContainer>
-                    </Box>
+                                        <TableRow>
+                                            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                                                <Collapse in={openRows[index]} timeout="auto" unmountOnExit>
+                                                    <Box sx={{ margin: 1 }}>
+                                                        <Typography
+                                                            variant="h6"
+                                                            gutterBottom
+                                                            component="div"
+                                                            sx={{ whiteSpace: 'pre-line' }}
+                                                        >
+                                                            Details:
+                                                        </Typography>
+                                                        <Typography variant="body1">
+                                                            Course Description: {course.courseDescription}
+                                                        </Typography>{' '}
+                                                        <br />
+                                                        <Typography variant="body1">
+                                                            Marker Responsibilities: {course.markerResponsibilities}
+                                                        </Typography>{' '}
+                                                        <br />
+                                                        <Typography variant="body1">
+                                                            Marker Hours: {course.markerHours}
+                                                        </Typography>
+                                                    </Box>
+                                                </Collapse>
+                                            </TableCell>
+                                        </TableRow>
+                                    </>
+                                ))}
+                                {emptyRows > 0 && (
+                                    <TableRow style={{ height: 69.5 * emptyRows }}>
+                                        <TableCell colSpan={6} />
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                        <TablePagination
+                            component="div"
+                            sx={{ width: '100%' }}
+                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            colSpan={3}
+                            count={data.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                            showFirstButton={true}
+                            showLastButton={true}
+                        />
+                    </TableContainer>
                 </Box>
             </Box>
         </ThemeProvider>
