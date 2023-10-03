7271f874b36fcf3347b30ee5238a8d1a262c2020
HaichuanZ <haichuanzhang.1028@gmail.com>
2023-10-03 18:30:07 +1300

    feature: revised base pie chart

--- src/app/components/CoordinatorDashboardData/CoursePieChart.tsx
+++ src/app/components/CoordinatorDashboardData/CoursePieChart.tsx
@@ -1,19 +1,6 @@
 import { PieChart } from '@mui/x-charts/PieChart'
 import { useEffect, useState } from 'react'
 
-interface Course {
-    id: number
-    courseCode: string
-    courseDescription: string
-    numOfEstimatedStudents: number
-    numOfEnrolledStudents: number
-    markerHours: number
-    markerResponsibilities: string
-    needMarkers: boolean
-    markersNeeded: number
-    semester: string
-}
-
 interface ChartData {
     id: number
     value: number
@@ -23,38 +10,50 @@ interface ChartData {
 export default function CoursePieChart() {
     const [applicationStats, setApplicationStats] = useState<ChartData[]>([])
 
-    useEffect(() => {
-        fetchData()
-    }, [])
-
-    const fetchData = async () => {
+    const fetchCoursesData = async () => {
         try {
             // get all courses
             const response = await fetch('/api/courses', { method: 'GET' })
             const coursesJson = await response.json()
-            for (let course in coursesJson) {
-                console.log(coursesJson[course])
-                let courseId = coursesJson[course].id.toString()
-                try {
-                    // get all applications for each course based on courseId (PK)
-                    const response = await fetch('/api/courses/' + courseId + '/applications', { method: 'GET' })
-                    const applicationsJson = await response.json()
-                    setApplicationStats((prev) => [
-                        ...prev,
-                        {
-                            id: coursesJson[course].id,
-                            value: applicationsJson.length,
-                            label: coursesJson[course].courseCode,
-                        },
-                    ])
-                } catch (error) {
-                    console.error('Error fetching data:', error)
-                }
-            }
+            return coursesJson
         } catch (error) {
             console.error('Error fetching data:', error)
         }
     }
+
+    const fetchApplications = async (courseId: number) => {
+        try {
+            // get all applications for each course based on courseId (PK)
+            const response = await fetch('/api/courses/' + courseId.toString() + '/applications', { method: 'GET' })
+            const applicationsJson = await response.json()
+            return applicationsJson
+        } catch (error) {
+            console.error('Error fetching data:', error)
+        }
+    }
+
+    useEffect(() => {
+        fetchCoursesData().then(async (courses) => {
+            const stats: ChartData[] = []
+
+            for (const course of courses) {
+                const courseId = course.id
+                const applications = await fetchApplications(courseId)
+                const numOfApplications = applications.length
+                const courseCode = course.courseCode
+
+                const courseData = {
+                    id: courseId,
+                    value: numOfApplications,
+                    label: courseCode,
+                }
+
+                stats.push(courseData)
+            }
+
+            setApplicationStats(stats) // Update the state with the fetched data
+        })
+    }, [])
 
     return (
         <PieChart
