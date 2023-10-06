14e139824f1dfa38d1dbcd5680614efcf6ab8543
ghxstling <dcho282@aucklanduni.ac.nz>
2023-10-06 22:15:49 +1300

    repo: implement test cases

--- src/data/supervisorRepo.test.ts
+++ src/data/supervisorRepo.test.ts
@@ -1,13 +1,22 @@
 import prisma from '@/libs/prisma'
 import UserRepo from './userRepo'
 import SupervisorRepo from './supervisorRepo'
+import { resetDatabase } from '@/helpers/testHelper'
 
-beforeEach( async () => {
+beforeAll(async () => {
+    await resetDatabase()
+})
+
+beforeEach(async () => {
     await prisma.user.deleteMany()
     await prisma.supervisor.deleteMany()
 })
 
-describe('StudentRepo', () => {
+afterAll(async () => {
+    await resetDatabase()
+})
+
+describe('SupervisorRepo', () => {
     it('can create a supervisor', async () => {
         const email = 'example@gmail.com'
         const userInput = { email }
@@ -16,7 +25,8 @@ describe('StudentRepo', () => {
             userId: user.id
         }
         const result = await SupervisorRepo.createSupervisor(supervisorInput)
-        expect(await SupervisorRepo.getSupervisorbyEmail(email)).toMatchObject(result)
+        const supervisor = await SupervisorRepo.getSupervisorbyEmail(email)
+        expect(supervisor).toMatchObject(result)
     })
     it('can create a supervisor from email', async () => {
         const email = 'example@gmail.com'
@@ -25,5 +35,44 @@ describe('StudentRepo', () => {
         const supervisorInput = {
             userId: user.id
         }
+        const result = await SupervisorRepo.createSupervisorFromEmail(email, supervisorInput)
+        const supervisor = await SupervisorRepo.getSupervisorbyEmail(email)
+        expect(supervisor).toMatchObject(result)
+    })
+    it('can get a supervisor by their user\'s email', async () => {
+        const email = 'example@gmail.com'
+        const userInput = { email }
+        const user = await UserRepo.createUser(userInput)
+        const supervisorInput = {
+            userId: user.id
+        }
+        const result = await SupervisorRepo.createSupervisorFromEmail(email, supervisorInput)
+        const supervisor = await SupervisorRepo.getSupervisorbyEmail(email)
+        expect(supervisor).toMatchObject(result)
+        expect(supervisor?.userId).toBe(user.id)
+
+        const email2 = 'example2@gmail.com'
+        const result2 = await SupervisorRepo.getSupervisorbyEmail(email2)
+        expect(result2).toBe(null)
+    })
+    it('can get all supervisors', async () => {
+        let email = 'example1@gmail.com'
+        const userInput1 = { email }
+        const user1 = await UserRepo.createUser(userInput1)
+        const supervisorInput1 = {
+            userId: user1.id
+        }
+        const supervisor1 = await SupervisorRepo.createSupervisorFromEmail(email, supervisorInput1)
+
+        email = 'example2@gmail.com'
+        const userInput2 = { email }
+        const user2 = await UserRepo.createUser(userInput2)
+        const supervisorInput2 = {
+            userId: user2.id
+        }
+        const supervisor2 = await SupervisorRepo.createSupervisorFromEmail(email, supervisorInput2)
+
+        const result = await SupervisorRepo.getAllSupervisors()
+        expect(result).toMatchObject([supervisor1, supervisor2])
     })
 })
