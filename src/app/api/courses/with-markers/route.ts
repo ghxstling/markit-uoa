ad784e266d1fcdfe2510ab2b79fb629616111782
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2023-10-09 17:32:09 +1300

    fix: move expensive endpoint to somewhere else

---
+++ src/app/api/courses/with-markers/route.ts
@@ -0,0 +1,13 @@
+import CourseRepo from '@/data/courseRepo'
+import CourseService from '@/services/courseService'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+    const courses = await CourseRepo.getAllCourses()
+    const newCourses = await CourseService.createCourseObjecs(courses)
+
+    return NextResponse.json(newCourses, {
+        status: 200,
+        statusText: 'OK',
+    })
+}
