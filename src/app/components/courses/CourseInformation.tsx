a0e3c7270965245053f4ee445fe70acd05031821
TheFatCat <74298130+TheFatCat27@users.noreply.github.com>
2023-10-12 02:03:50 +1300

    fix

--- src/app/components/courses/CourseInformation.tsx
+++ src/app/components/courses/CourseInformation.tsx
@@ -694,7 +694,7 @@ const CourseInformation = ({ courseId }: CourseInformationProps) => {
                                         <Checkbox
                                             checked={checkedStudents.includes(application.studentId) || false}
                                             onChange={() => handleCheckedStudents(application.studentId)}
-                                            disabled={application.applicationStatus === 'denied' && application.applicationStatus === 'denied'}
+                                            disabled={application.applicationStatus === 'denied' || application.applicationStatus === 'approved'}
                                         />
                                     </TableCell>
                                     <TableCell style={{ textAlign: 'center' }}>
