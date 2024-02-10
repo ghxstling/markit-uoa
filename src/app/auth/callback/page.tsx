b37bcb2f084ef86984eafc8f9866dc14284cb687
ghxstling <dcho282@aucklanduni.ac.nz>
2024-02-10 23:21:17 +1300

    refactor: prettified callback page

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -2,6 +2,7 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
+import { CircularProgress, LinearProgress, Typography, Box, Container } from '@mui/material'
 
 function AuthCallback() {
     const { data: session } = useSession()
@@ -13,7 +14,30 @@ function AuthCallback() {
         }
     }, [session, router])
 
-    return <div>Redirecting...</div>
+    return (
+        <>
+            <Container
+                maxWidth={false}
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    height: '65vh',
+                    textAlign: 'center',
+                    maxWidth: '15vw',
+                }}
+            >
+                <Typography variant="overline" fontSize={'1vw'} align="center" gutterBottom>
+                    Redirecting...
+                </Typography>
+                <LinearProgress
+                    sx={{
+                        height: '0.25vw',
+                    }}
+                />
+            </Container>
+        </>
+    )
 }
 
 export default AuthCallback
